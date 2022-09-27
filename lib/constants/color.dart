acd69fe3dc26c65e96d571ebb3e434af0fe17914
ehsanyaqoob <ehsanyaqoob07@gmail.com>
2022-09-27 09:42:42 +0500

    initial commit

---
+++ lib/constants/color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color toRed = Color(0xffda4040);
+
+const Color toBlue = Color(0xff5f52ee);
+
+const Color toBlack = Color(0xfff3a3a3a);
+
+const Color toGrey = Color(0xff717171);
+
+const Color tdBGColor = Color(0xffeeeff5);
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:todo_app/screens/homescreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
